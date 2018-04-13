f92c5c3353c8940c6540fa693e6d64d6be1b6fc4
Calebe Emerick <hello@calebeemerick.com>
2018-04-13 12:10:03 -0300

    Add a example showing how to use the BKJSONEncoder and also the regular way.

--- BKJSONEncoder/Example/ExampleController.swift
+++ BKJSONEncoder/Example/ExampleController.swift
@@ -8,4 +8,48 @@
 
 import UIKit
 
-final class ExampleController: UIViewController {}
+final class ExampleController: UIViewController {
+    
+    struct Pet: Codable {
+        
+        let name: String
+        let age: Int
+        let type: String
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Using BKJSONEncoder
+        
+        
+        let encoder = BKJSONEncoder()
+        let myCat = Pet(name: "Bill", age: 2, type: "Cat")
+        
+        do {
+            let pet = try encoder.encode(myCat)
+            print(pet)
+        }
+        catch let error {
+            print(error)
+        }
+        
+        
+        
+        // NOT Using BKJSONEncoder
+        
+        
+        let defaultEncoder = JSONEncoder()
+        let myDog = Pet(name: "Bira", age: 33, type: "Dog")
+        
+        do {
+            let encodedPet = try defaultEncoder.encode(myDog)
+            let jsonObject = try JSONSerialization.jsonObject(with: encodedPet, options: [])
+            let jsonPet = jsonObject as! JSON
+            print(jsonPet)
+        }
+        catch let error {
+            print(error)
+        }
+    }
+}
